f67ac88748754d327fb5f986001d27870e105908
Xiobe <3995581+Xiobe@users.noreply.github.com>
2023-09-12 11:55:52 +0200

    Create dnsinfo.py

---
+++ dnsinfo.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python3
+
+__version__ = "0.0.0"
+__prog__ = f"dnsinfo.py {__version__}"
+__description__ = 'DNS information'
+
+
+class DNSinfo:
+    pass
+
+if __name__ == '__main__':
+    main()
